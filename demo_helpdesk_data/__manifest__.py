3e70a30e09b7a86320806bed28b096d4305816f8
Mathieu Benoit <mathben@technolibre.ca>
2021-01-05 16:46:03 -0500

    [ADD] code_generator_demo_export_helpdesk with demo_helpdesk_data

---
+++ demo_helpdesk_data/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Demo helpdesk data',
+    'version': '12.0.1.0',
+    'author': 'TechnoLibre',
+    'website': 'https://technolibre.ca',
+    'license': 'AGPL-3',
+    'depends': ['helpdesk_mgmt'],
+    'data': ['data/helpdesk_ticket.xml'],
+    'installable': True,
+}

0e11bf0262def0d2069bb03176c376d2acc52119
Mathieu Benoit <mathben@technolibre.ca>
2021-01-05 16:43:43 -0500

    [ADD] code_generator_demo_internal with web view

---
+++ code_generator_demo_internal/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Setup testing environment for internal view code generator',
+    'author': 'TechnoLibre',
+    'license': 'AGPL-3',
+    'depends': [
+        'code_generator',
+    ],
+    'data': [],
+    'installable': True,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
+}
---
+++ demo_internal/models/demo_model_2_internal.py
@@ -0,0 +1,14 @@
+from odoo import _, api, models, fields
+
+
+class DemoModel2Internal(models.Model):
+    _name = 'demo.model_2.internal'
+    _description = 'demo_model_2_internal'
+
+    model_1 = fields.Many2one(
+        string='Model 1',
+        comodel_name='demo.model.internal',
+        on_delete='set null',
+    )
+
+    name = fields.Char(string='Name')

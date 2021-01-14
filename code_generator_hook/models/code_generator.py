fe78160dff1e180714748a355ad7db3b000176ff
Mathieu Benoit <mathben@technolibre.ca>
2021-01-14 00:40:17 -0500

    [FIX] code_generator_hook: generate hook from a demo code generator - Add constant hook in begin of hook file - Support generate hook demo

--- code_generator_hook/models/code_generator.py
+++ code_generator_hook/models/code_generator.py
@@ -4,6 +4,9 @@ from odoo import models, fields, api, modules, tools
 class CodeGeneratorModule(models.Model):
     _inherit = 'code.generator.module'
 
+    # TODO rename variable with hook_*
+    hook_constant_code = fields.Text(string="Code constant", help="Code in the begin of hook file.")
+
     # pre_init_hook
     pre_init_hook_show = fields.Boolean(string="Show pre_init_hook")
 
@@ -21,6 +24,10 @@ class CodeGeneratorModule(models.Model):
         string="General conf post_init_hook",
         help="Add code to update general configurations on post_init_hook.")
 
+    post_init_hook_feature_code_generator = fields.Boolean(
+        string="Code generator post_init_hook",
+        help="Add code to use the code generator on post_init_hook.")
+
     post_init_hook_code = fields.Text(string="Code of post_init_hook", default="""with api.Environment.manage():
     env = api.Environment(cr, SUPERUSER_ID, {})""")
 
@@ -33,3 +40,6 @@ class CodeGeneratorModule(models.Model):
 
     uninstall_hook_code = fields.Text(string="Code of uninstall_hook", default="""with api.Environment.manage():
     env = api.Environment(cr, SUPERUSER_ID, {})""")
+
+    uninstall_hook_feature_code_generator = fields.Boolean(
+        string="Code generator uninstall_hook", help="Add code to use the code generator on uninstall_hook.")
--- code_generator_hook/models/code_generator_writer.py
+++ code_generator_hook/models/code_generator_writer.py
@@ -20,7 +20,8 @@ class CodeGeneratorWriter(models.Model):
                 lst_import.append("post_init_hook")
             if module.uninstall_hook_show:
                 lst_import.append("uninstall_hook")
-            self.code_generator_data.add_module_init_path(f'from .hooks import {", ".join(lst_import)}')
+            # Specify root component
+            self.code_generator_data.add_module_init_path('', f'from .hooks import {", ".join(lst_import)}')
 
     def set_manifest_file_extra(self, cw, module):
         super(CodeGeneratorWriter, self).set_manifest_file_extra(cw, module)
@@ -46,8 +47,16 @@ class CodeGeneratorWriter(models.Model):
             str_line = line.strip()
             cw.emit(str_line)
 
-        def _add_hook(cw, hook_show, hook_code, hook_feature_gen_conf, method_name, has_second_arg):
+        # Add constant
+        if module.hook_constant_code:
+            for line in module.hook_constant_code.split('\n'):
+                cw.emit(line)
+
+        def _add_hook(cw, hook_show, hook_code, hook_feature_gen_conf, post_init_hook_feature_code_generator,
+                      uninstall_hook_feature_code_generator,
+                      method_name, has_second_arg):
             if hook_show:
+                cw.emit()
                 cw.emit()
                 if has_second_arg:
                     cw.emit(f'def {method_name}(cr, e):')
@@ -64,14 +73,74 @@ class CodeGeneratorWriter(models.Model):
 
                             cw.emit("event_config = env['res.config.settings'].sudo().create(values)")
                             cw.emit("event_config.execute()")
-                cw.emit()
+                    if post_init_hook_feature_code_generator:
+                        with cw.indent():
+                            cw.emit()
+                            cw.emit("# path_module_generate = os.path.normpath(os.path.join(os.path.dirname"
+                                    "(__file__), '..'))")
+                            cw.emit()
+                            cw.emit('short_name = MODULE_NAME.replace("_", " ").title()')
+                            cw.emit()
+                            cw.emit("# Add code generator")
+                            cw.emit("value = {")
+                            with cw.indent():
+                                cw.emit('"shortdesc": short_name,')
+                                cw.emit("\"name\": MODULE_NAME,")
+                                cw.emit("\"license\": \"AGPL-3\",")
+                                cw.emit("\"author\": \"TechnoLibre\",")
+                                cw.emit("\"website\": \"https://technolibre.ca\",")
+                                cw.emit("\"application\": True,")
+                                # with cw.block(before='"depends" :', delim=('[', '],')):
+                                #     cw.emit('"code_generator",')
+                                #     cw.emit('"code_generator_hook",')
+                                cw.emit("\"enable_sync_code\": True,")
+                                cw.emit("# \"path_sync_code\": path_module_generate,")
+                            cw.emit("}")
+                            cw.emit()
+                            cw.emit("value[\"post_init_hook_show\"] = True")
+                            cw.emit("value[\"uninstall_hook_show\"] = True")
+                            cw.emit("value[\"post_init_hook_feature_code_generator\"] = True")
+                            cw.emit("value[\"uninstall_hook_feature_code_generator\"] = True")
+                            cw.emit("value[\"hook_constant_code\"] = f'MODULE_NAME = \"{MODULE_NAME}\"'")
+                            cw.emit()
+                            cw.emit("code_generator_id = env[\"code.generator.module\"].create(value)")
+                            cw.emit()
+                            cw.emit("# Add dependencies")
+                            with cw.block(before='lst_depend =', delim=('[', ']')):
+                                cw.emit("\"code_generator\",")
+                                cw.emit("\"code_generator_hook\",")
+                            cw.emit(
+                                "lst_dependencies = env[\"ir.module.module\"].search([(\"name\", \"in\", lst_depend)])")
+                            cw.emit("for depend in lst_dependencies:")
+                            with cw.indent():
+                                with cw.block(before='value =', delim=('{', '}')):
+                                    cw.emit("\"module_id\": code_generator_id.id,")
+                                    cw.emit("\"depend_id\": depend.id,")
+                                    cw.emit("\"name\": depend.display_name,")
+                                cw.emit("env[\"code.generator.module.dependency\"].create(value)")
+                            cw.emit()
+                            cw.emit("# Generate module")
+                            cw.emit("value = {")
+                            with cw.indent():
+                                cw.emit("\"code_generator_ids\": code_generator_id.ids")
+                            cw.emit("}")
+                            cw.emit("code_generator_writer = env[\"code.generator.writer\"].create(value)")
+                    if uninstall_hook_feature_code_generator:
+                        with cw.indent():
+                            cw.emit(
+                                'code_generator_id = env["code.generator.module"].search([("name", "=", MODULE_NAME)])')
+                            cw.emit('if code_generator_id:')
+                            with cw.indent():
+                                cw.emit('code_generator_id.unlink()')
 
         _add_hook(cw, module.pre_init_hook_show, module.pre_init_hook_code,
-                  module.pre_init_hook_feature_general_conf, "pre_init_hook", False)
+                  module.pre_init_hook_feature_general_conf, False, False, "pre_init_hook", False)
         _add_hook(cw, module.post_init_hook_show, module.post_init_hook_code,
-                  module.post_init_hook_feature_general_conf, "post_init_hook", True)
+                  module.post_init_hook_feature_general_conf, module.post_init_hook_feature_code_generator, False,
+                  "post_init_hook", True)
         _add_hook(cw, module.uninstall_hook_show, module.uninstall_hook_code,
-                  module.uninstall_hook_feature_general_conf, "uninstall_hook", True)
+                  module.uninstall_hook_feature_general_conf, False, module.post_init_hook_feature_code_generator,
+                  "uninstall_hook", True)
 
         hook_file_path = 'hooks.py'
 

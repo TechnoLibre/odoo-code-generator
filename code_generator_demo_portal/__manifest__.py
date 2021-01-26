e5081f9e2d4d0131a0b4bd7a5a0928333e5f471f
Mathieu Benoit <mathben@technolibre.ca>
2021-01-26 05:45:45 -0500

    [FIX] demo_generator_demo_portal update - Add multiple type field demo portal - Missing monetarize

--- code_generator_demo_portal/__manifest__.py
+++ code_generator_demo_portal/__manifest__.py
@@ -1,15 +1,16 @@
-# -*- coding: utf-8 -*-
-
 {
-    'name': 'Setup testing environment for portal code generator',
+    'name': 'Code Generator Demo Portal',
+    'version': '12.0.1.0',
     'author': 'TechnoLibre',
+    'website': 'https://technolibre.ca',
     'license': 'AGPL-3',
+    'application': True,
     'depends': [
         'code_generator',
+        'code_generator_hook',
         'code_generator_portal',
     ],
-    'data': [],
     'installable': True,
-    "post_init_hook": "post_init_hook",
-    "uninstall_hook": "uninstall_hook",
+    'post_init_hook': 'post_init_hook',
+    'uninstall_hook': 'uninstall_hook',
 }

db41ee79677da834aed3b6d8a058de9b347231d0
Mathieu Benoit <mathben@technolibre.ca>
2021-02-15 12:45:55 -0500

    [ADD] code_generator_website_snippet

---
+++ code_generator_website_snippet/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Code Generator - website snippet generation',
+    'version': '12.0.0',
+    'summary': 'Code Generator - Create website snippet',
+    'description': 'Code generator builder to create website snippet',
+    'author': 'Mathben (mathben@technolibre.ca)',
+    'category': 'Extra Tools',
+    'depends': [
+        'code_generator',
+    ],
+    'installable': True,
+    'data': [
+    ]
+}

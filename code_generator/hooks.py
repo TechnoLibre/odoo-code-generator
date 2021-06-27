7a4b2303072e461a7dda7506044eca84f3b0c27b
Mathieu Benoit <mathben@technolibre.ca>
2021-06-27 01:25:35 -0400

    [UPD] code_generator installation need --dev argument for protection

--- code_generator/hooks.py
+++ code_generator/hooks.py
@@ -1,7 +1,15 @@
-from odoo import _, api, models, fields, SUPERUSER_ID
+from odoo import _, api, models, fields, SUPERUSER_ID, tools
 
 
 def post_init_hook(cr, e):
+    if not tools.config["dev_mode"]:
+        raise Exception(
+            _(
+                "Cancel installation module code_generator, please specify"
+                " --dev [options] in your instance."
+            )
+        )
+
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
         system_user = env["res.users"].browse(2)
